b043abf7c612dd8a5c756130f7d41f4a2ef7bb9c
Haoran He <69794814+tinnerhrhe@users.noreply.github.com>
2024-03-07 09:54:12 +0800

    Update __init__.py

--- diffuser/models/__init__.py
+++ diffuser/models/__init__.py
@@ -1,3 +1,3 @@
-from .temporal import TemporalUnet, Tasksmaze, TasksUnet, TasksOnehot, Taskstext, TasksmetaAug, TasksAug, Tasksmeta, TasksTemporalUnet, TasksAttTemporalUnet, ValueFunction, TasksDit, TasksDT, TasksActDT, TasksDT_v1, EmbTasksAttTemporalUnet
+from .temporal import Tasksmaze, TasksmetaAug, Tasksmeta
 from .diffusion import GaussianDiffusion, AugDiffusion, GaussianActDiffusion,  GaussianInvDiffusion
 #from .MTDT import DecisionTransformer, PromptDT, PromptDTMaze
